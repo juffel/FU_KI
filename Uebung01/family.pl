2466c3b4e10c1e25c7ee87ac6a1e3e3352fb5345
Julian Dobmann <julian.dobmann@gmail.com>
2015-04-17 16:21:06 +0200

    ein paar einfache Familienbeziehungen mit griechischer Mythologie umgesetzt.

---
+++ Uebung01/family.pl
@@ -0,0 +1,57 @@
+% family tree prolog exercise
+% disclaimer: the relations between the following individuals are considered on a purely biological basis 
+%%%%%%%%%%%%%
+% individuals
+zeus.
+hades.
+poseidon.
+demeter.
+hestia.
+
+ares.
+
+%%%%%
+% sex
+male(zeus).
+male(hades).
+male(poseidon).
+male(ares).
+female(demeter).
+female(hestia).
+female(hera).
+
+%%%%%%%%%%%%%%%%
+% zeus' siblings
+brother(zeus,hera).
+brother(zeus,hades).
+brother(zeus,hestia).
+brother(zeus,poseidon).
+brother(zeus,demeter).
+
+%%%%%%%%%%%%%%%%
+% zeus' begattings in the form parents(parent_1, parent_2, child)
+parents(zeus,hera,ares).
+parents(zeus,hera,hephaistos).
+parents(zeus,hera,hebe).
+parents(zeus,hera,eileithyia).
+parents(zeus,leto,artemis).
+parents(zeus,leto,apollon).
+parents(zeus,maia,hermes).
+parents(zeus,methis,athene).
+parents(zeus,demeter,persephone).
+parents(zeus,dione,aphrodite).
+% zeus' mortal inseminations
+parents(zeus,alkmene,herakles).
+parents(zeus,semele,dionysos).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% family structure definitions
+parent(X,Y) :- parents(X,_,Y);parents(_,X,Y).
+father(X,Y) :- parent(X,Y),male(X).
+mother(X,Y) :- parent(X,Y),female(X).
+
+% two individuals are considered siblings if they share at least a parent
+sibling(X,Y) :- parent(Z,X),parent(Z,Y).
+
+% TODO ancestor, predecessor
+
