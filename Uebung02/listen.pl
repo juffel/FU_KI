4226a37e7a10a9530948464ceefe4b34ea545d83
Kai Kruschel <kaikruschel@googlemail.com>
2015-05-08 13:00:02 +0200

    myOrder implementiert, leider nur in eine Richtung

--- Uebung02/listen.pl
+++ Uebung02/listen.pl
@@ -16,3 +16,33 @@ mySum([X], Y):-
 
 mySum([Head | Tail], X):-
   mySum(Tail, (X - Head)).
+
+# myOrder ufert ein bisschen aus...
+myOrder([X, Y]):-
+  X >= Y.
+
+myOrder([X, Y]):-
+  X =< Y.
+
+myOrder([X, Y | Rest]):-
+  X >= Y,
+  myOrderGreater(Y | Rest).
+
+myOrder([X, Y | Rest]):-
+  X =< Y,
+  myOrderLesser([Y | Rest]).
+
+myOrderGreater([X | [Y]]):-
+  X >= Y.
+
+#Aus irgendeinem Grund wird das immer zu false ausgewertet
+myOrderGreater([X, Y | Rest]):-
+  X >= Y,
+  myOrderGreater([Y | Rest]).
+
+myOrderLesser([X | [Y]]):-
+  X =< Y.
+
+myOrderLesser([X, Y | Rest]):-
+  X =< Y,
+  myOrderLesser([Y | Rest]).
