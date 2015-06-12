58839e641b46b6509bd498782353ce502437091a
Kai Kruschel <kaikruschel@googlemail.com>
2015-06-12 12:21:00 +0200

    a und b loofen

--- Uebung07/erster_Versuch.pl
+++ Uebung07/erster_Versuch.pl
@@ -5,14 +5,15 @@ connection(b, e).
 connection(c, d).
 connection(c, e).
 
-connection(X, Y):-
-connection(Y, X).
+connected(X, Y):-
+  connection(X, Y);
+  connection(Y, X).
 
 
 path(X, Y, P):-
-  connection(X, Y),
+  connected(X, Y),
   write([X, Y] | P).
 
 path(X, Y, P):-
-  connection(Z, Y),
+  connected(Z, Y),
   path(X, Z, [[Z, Y] | P]).
