dfe59de201c6c25907beb30fa0630f353b8257be
Julian Dobmann <julian.dobmann@gmail.com>
2015-06-26 11:21:08 +0200

    Übung 9 Aufgabe 2 a) "gelöst". Lösung funktioniert leider nicht, verstehe aber nicht warum :/

---
+++ Uebung09/heuristik.pl
@@ -0,0 +1,18 @@
+% heuristik ermittelt zum Zustand X die Anzahl N der nicht mit dem Zielzustand Z übereinstimmenden Zahlenfelder
+% (implizite Bedingung: X.size == Z.size)
+% es geht dabei um das 3x3 Schiebepuzzle
+% ein Zustand hat dabei die Form board(1,2,3,4,5,6,7,8,b)
+
+% der Abstand zweier ein-elementiger Listen ist 0, wenn sie das gleiche
+% Element enthalten, und 1 wenn sie unterschiedliche Elemente enthalten
+heuristik([X],[Z],N) :-
+    (X == Z), N is 0;
+    (X \= Z), N is 1.
+% der Abstand zweier mehr-elementiger Listen mit gleichem Head ist
+% 0 + Abstand der Restlisten
+% der Absatnd zweier mehr-elementiger Listen mit verschiedenem Head
+% ist 1 + Abstand der Restlisten
+heuristik([Xh|X],[Zh|Z],N) :-
+    (Xh == Zh), heuristik(X,Z,N);
+    (Xh \= Zh), heuristik(X,Z,N-1).
+
